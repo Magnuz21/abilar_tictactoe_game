4b3e029b5ef78ec51067ea3a764a79c7a061531c
Aldrin Chicote Abilar <101628771+Magnuz21@users.noreply.github.com>
2023-03-11 03:40:39 +0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,127 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import React, { useState } from 'react';
+import { Board } from '../components/Board';
+import { ChoosePlayer } from '../components/ChoosePlayer';
+import { WinnerModal } from '../components/WinnerModal';
+
+
+const Home: NextPage = () => {
+
+  const [isX, setIsX] = useState<boolean>(true);
+  const [newGame, setNewGame] = useState<boolean>(false);
+  const [squares, setSqaures] = useState<Array<any>>(Array(9).fill(null));
+
+  let winner = calculateWinner(squares);
+
+
+  // handle Choose player
+  function handlePlayerX() {
+    setIsX(true);
+  }
+
+  function handlePlayerO() {
+    setIsX(false);
+
+  }
+
+  //// It will Handle which Icon will apppear on Board on cliking  one the Squares
+  function handlePlayer(i: number) {
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = isX ? "X" : "O";
+    setSqaures(squares);
+    setIsX(!isX);
+  }
+
+  // It will handle the Restart of the Game 
+
+  function handleRestartGame() {
+    setIsX(true);
+    setSqaures(Array(9).fill(null));
+  }
+
+
+  // It will handle the start Game when the player choose one of the Icon
+  // with which they want to player
+  function handleNewGame() {
+    setIsX(true);
+    setSqaures(Array(9).fill(null));
+    setNewGame(true);
+  };
+
+  function handleQuitGame() {
+    setIsX(true);
+    setSqaures(Array(9).fill(null));
+    setNewGame(false);
+  }
+  // Calculate the winner
+  function calculateWinner(squares: Array<any>) {
+    // Total 8 winning patterens
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [a, b, c] = winningPatterns[i];
+
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
+  return (
+    <div className="flex min-h-screen bg-[#004b23] flex-col items-center  py-2">
+      <Head>
+        <title>Abilar-Tic-Tic-Toe Game</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="text-4xl md:text-6xl font-extrabold mt-4 text-[#ffbe0b] ">
+        Abilar's
+        {" "}
+        <span className="text-[#fb5607]">TicTacToe </span>
+        {" "}
+        <span className="text-[#ff006e]">Game</span>
+      </h1>
+
+      {!newGame
+        ?
+        <ChoosePlayer
+          handleNewGame={handleNewGame}
+          handlePlayerX={handlePlayerX}
+          handlePlayerO={handlePlayerO}
+        />
+        :
+        <Board
+          winner={winner}
+          playerX={isX}
+          squares={squares}
+          handlePlayer={handlePlayer}
+          handleRestartGame={handleRestartGame}
+        />
+      }
+      {winner && 
+      <WinnerModal
+          winner={winner}
+          handleQuitGame={handleQuitGame}
+          handleNewGame={handleNewGame}
+        />
+      }
+    </div>
+  )
+}
+
+export default Home
